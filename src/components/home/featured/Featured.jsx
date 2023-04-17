30eed6e46f4e592877cfc6ae3a099570520765ec
qxan <mehmet.enfa@gmail.com>
2023-04-17 16:25:30 +0300

    featured

--- src/components/home/featured/Featured.jsx
+++ src/components/home/featured/Featured.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Heading from "../../common/Heading";
+import "./Featured.css";
+import FeaturedCard from "./FeaturedCard";
 
 const Featured = () => {
   return (
-    <div>Featured</div>
-  )
-}
+    <>
+      <section className="featured background">
+        <div className="container">
+          <Heading />
+          <FeaturedCard
+            title="Featured Property Types"
+            subtitle="Find All Type of Property."
+          />
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Featured
+export default Featured;
