85652088152b3deb4dcaf8226405258f8f0e3282
qxan <mehmet.enfa@gmail.com>
2023-04-18 15:51:23 +0300

    awardsFile

--- src/components/home/awards/Awards.jsx
+++ src/components/home/awards/Awards.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Heading from "../../common/Heading";
+import { awards } from "../../data/Data";
+import "./awards.css";
 
 const Awards = () => {
   return (
-    <div>Awards</div>
-  )
-}
+    <>
+      <section className="awards padding">
+        <div className="container">
+          <Heading
+            title="Over 1,24,000+ Happy User Bieng With Us Still They Love Our Services"
+            subtitle="Our Awards"
+          />
 
-export default Awards
+          <div className="content grid4 mtop">
+            {awards.map((val, index) => (
+              <div className="box" key={index}>
+                <div className="icon">
+                  <span>{val.icon}</span>
+                </div>
+                <h1>{val.num}</h1>
+                <hp>{val.name}</hp>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Awards;
