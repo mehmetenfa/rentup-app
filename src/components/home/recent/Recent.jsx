f3a357f68400e8f17b6a442df47377af8a1aae68
qxan <mehmet.enfa@gmail.com>
2023-04-18 15:29:38 +0300

    recent

--- src/components/home/recent/Recent.jsx
+++ src/components/home/recent/Recent.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Heading from "../../common/Heading";
+import "./recent.css";
+import RecentCard from "./RecentCard";
 
 const Recent = () => {
   return (
-    <div>Recent</div>
-  )
-}
+    <>
+      <section className="recent padding">
+        <div className="container">
+          <Heading
+            title="Recent Property Listed"
+            subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          />
+          <RecentCard />
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Recent
+export default Recent;
